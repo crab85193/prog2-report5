e6e5f59d8aad2b8f6cf44523434dcb0a80891d6f
crab85193 <crab85193@gmail.com>
2021-12-10 17:37:49 +0900

    [Update] Change the position of close().
    Updated to the code in Textbook 17-4 and confirmed that it works.

--- src/jp/ac/uryukyu/ie/e215726/Code17.java
+++ src/jp/ac/uryukyu/ie/e215726/Code17.java
@@ -4,12 +4,13 @@ import java.io.*;
 
 public class Code17 {
     public static void main(String[] args){
+        FileWriter fw = null;
         try{
-            FileWriter fw = new FileWriter("data.txt");
+            fw = new FileWriter("data.txt");
             fw.write("hello!");
-            fw.close();
         }catch(Exception e){
-            System.out.println("何らかの例外が発生しました。");
+            System.out.println("エラーです。");
         }
+        fw.close();
     }
 }

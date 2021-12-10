762b73cacbff53eff3358f757fac15fc243d2ce4
crab85193 <crab85193@gmail.com>
2021-12-10 17:49:25 +0900

    [Update] Change the scope of the fw variable
    Updated to the code in Textbook 17-6 and confirmed that it works.

--- src/jp/ac/uryukyu/ie/e215726/Code17.java
+++ src/jp/ac/uryukyu/ie/e215726/Code17.java
@@ -4,8 +4,8 @@ import java.io.*;
 
 public class Code17 {
     public static void main(String[] args){
+        FileWriter fw = new FileWriter("data.txt");
         try{
-            FileWriter fw = new FileWriter("data.txt");
             fw.write("hello!");
         }catch(Exception e){
             System.out.println("エラーです。");

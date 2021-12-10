058a8bd1bce3fe49417ec3265c74d620573fbd7f
crab85193 <crab85193@gmail.com>
2021-12-10 16:59:49 +0900

    [Update] Add exception handling.
    This is the commit of step 2.
    Added exception handling to simply show which errors were encountered.

--- src/jp/ac/uryukyu/ie/e215726/Main.java
+++ src/jp/ac/uryukyu/ie/e215726/Main.java
@@ -2,7 +2,13 @@ package jp.ac.uryukyu.ie.e215726;
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch(NullPointerException e){
+            System.out.println(e.getClass().getSimpleName() + "が発生");
+            System.out.println("-----MESSAGE-----");
+            System.out.println(e.getMessage());
+        }
     }
 }

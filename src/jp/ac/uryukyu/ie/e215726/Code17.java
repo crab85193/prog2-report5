19238743c51bbb9d536b1bc2c33e30a2d9920695
crab85193 <crab85193@gmail.com>
2021-12-10 18:05:03 +0900

    [Add & Update] Add Person.java and Update Code17.java
    Updated to the code in Textbook 17-11 and confirmed that it works.

--- src/jp/ac/uryukyu/ie/e215726/Code17.java
+++ src/jp/ac/uryukyu/ie/e215726/Code17.java
@@ -1,13 +1,8 @@
 package jp.ac.uryukyu.ie.e215726;
 
-import java.io.*;
-
 public class Code17 {
     public static void main(String[] args){
-        try(FileWriter fw = new FileWriter("data.txt");){
-            fw.write("hello!");
-        }catch(Exception e){
-            System.out.println("何らかの例外が発生しました。");
-        }
+        Person p = new Person();
+        p.setAge(-128);
     }
 }

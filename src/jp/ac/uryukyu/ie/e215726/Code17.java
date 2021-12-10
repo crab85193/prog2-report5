e8e41f7bab2a7d6edd59220ce79162bf8ab58ed8
crab85193 <crab85193@gmail.com>
2021-12-10 17:28:39 +0900

    [Update] Add exception handling.
    Updated to the code in Textbook 17-2 and added exception handling.

--- src/jp/ac/uryukyu/ie/e215726/Code17.java
+++ src/jp/ac/uryukyu/ie/e215726/Code17.java
@@ -4,6 +4,12 @@ import java.io.*;
 
 public class Code17 {
     public static void main(String[] args){
-        FileWriter fw = new FileWriter("data.txt");
+        try{
+            FileWriter fw = new FileWriter("data.txt");
+
+        }catch(IOException e){
+            System.out.println("エラーが発生しました。");
+
+        }
     }
 }

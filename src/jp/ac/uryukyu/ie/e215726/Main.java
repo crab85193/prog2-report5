f8256330c4e473cc9e26f476eeebf762f0dcd8af
crab85193 <crab85193@gmail.com>
2021-12-10 17:06:20 +0900

    [Update] Update the code in Step 3.
    This is the commit of step 3.
    Updated the code for Step 3.

--- src/jp/ac/uryukyu/ie/e215726/Main.java
+++ src/jp/ac/uryukyu/ie/e215726/Main.java
@@ -2,13 +2,7 @@ package jp.ac.uryukyu.ie.e215726;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch(NullPointerException e){
-            System.out.println(e.getClass().getSimpleName() + "が発生");
-            System.out.println("-----MESSAGE-----");
-            System.out.println(e.getMessage());
-        }
+        String str = "百二十三";
+        Integer.parseInt(str);
     }
 }

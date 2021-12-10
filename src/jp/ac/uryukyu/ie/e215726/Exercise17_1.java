dae83634ca0e127a5211c9e788b71cb5843b47ab
crab85193 <crab85193@gmail.com>
2021-12-10 18:20:09 +0900

    [Add] Add Exercise17_1.java
    Added the code for exercise 17-1 in the textbook

---
+++ src/jp/ac/uryukyu/ie/e215726/Exercise17_1.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.ie.e215726;
+
+public class Exercise17_1 {
+    public static void main(String[] args){
+        String s = null;
+        System.out.println(s.length());
+    }
+}

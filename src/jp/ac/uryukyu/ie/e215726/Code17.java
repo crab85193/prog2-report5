eb5a8fdaa55928f7dce570fb1ed22d642e1acb76
crab85193 <crab85193@gmail.com>
2021-12-10 18:09:19 +0900

    [Add & Update] Add UnsupportedMusicFileException.java and Define the original exception
    Updated to the code in Textbook 17-12 & 17-13 and confirmed that it works.

--- src/jp/ac/uryukyu/ie/e215726/Code17.java
+++ src/jp/ac/uryukyu/ie/e215726/Code17.java
@@ -2,7 +2,11 @@ package jp.ac.uryukyu.ie.e215726;
 
 public class Code17 {
     public static void main(String[] args){
-        Person p = new Person();
-        p.setAge(-128);
+        try{
+            // 試験的に例外を発生させる
+            throw new UnsupportedMusicFileException("未対応のファイルです");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }

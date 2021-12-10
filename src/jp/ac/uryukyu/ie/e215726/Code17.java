d61b6aaf9971367c7dafbf305655525f911ed4b6
crab85193 <crab85193@gmail.com>
2021-12-10 17:20:15 +0900

    [Add] Add Code17.java
    The commit after this is the code from the textbook example.
    Added the code for Textbook 17-1.

---
+++ src/jp/ac/uryukyu/ie/e215726/Code17.java
@@ -0,0 +1,9 @@
+package jp.ac.uryukyu.ie.e215726;
+
+import java.io.*;
+
+public class Code17 {
+    public static void main(String[] args){
+        FileWriter fw = new FileWriter("data.txt");
+    }
+}

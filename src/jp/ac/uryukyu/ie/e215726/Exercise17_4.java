a5dacadc59b954496892ef2ba6cdc64de38fc1de
crab85193 <crab85193@gmail.com>
2021-12-10 18:32:48 +0900

    [Add] Add Exercise17_4.java
    Added the code for exercise 17-4 in the textbook.

---
+++ src/jp/ac/uryukyu/ie/e215726/Exercise17_4.java
@@ -0,0 +1,15 @@
+package jp.ac.uryukyu.ie.e215726;
+
+import java.io.IOException;
+
+public class Exercise17_4 {
+    public static void main(String[] args) throws IOException{
+        System.out.println("起動");
+
+        throw new IOException();
+
+        // System.out.println("動作中");
+
+        // System.out.println("終了");
+    }
+}
